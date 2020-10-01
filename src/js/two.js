6ca798fd31fd9be96b5c70e4ac1506caee085839
philipfryxell <philipnordstrom@outlook.com>
2020-10-01 09:29:59 +0200

    Added more ES6

--- src/js/two.js
+++ src/js/two.js
@@ -3,4 +3,14 @@ console.log("Hello from two.js");
 
 hello = () => {
     return "Hello World!";
-  } 
+  }
+
+var number = [1, 2, 6, 8, 12, 14];
+
+var square = number.map(element => element*element)
+
+console.log(square);
+
+var result = number.filter(element => element>5)
+
+console.log(result);
