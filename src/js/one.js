50b9afe761da0ed8e72ce7241da40d95ac1ea0b4
philipfryxell <philipnordstrom@outlook.com>
2020-10-05 17:57:25 +0200

    Updated

--- src/js/one.js
+++ src/js/one.js
@@ -1,27 +1,122 @@
-const menuBtn = document.querySelector('.menu-btn');
-const hamburger = document.querySelector('.menu-btn__burger');
-const nav = document.querySelector('.nav');
-const menuNav = document.querySelector('.menu-nav');
-const navItems = document.querySelectorAll('.menu-nav__item');
+'use strict'
 
-let showMenu = false;
+// Variables
+let coursesEl = document.getElementById('courses');
+let addButton = document.getElementById('addButton');
+let updateDiv = document.getElementById('updateDiv');
+let code = document.getElementById('code');
+let name = document.getElementById('name');
+let prog = document.getElementById('prog');
+let plan = document.getElementById('plan');
 
-menuBtn.addEventListener('click', toggleMenu);
 
-function toggleMenu() {
-  if(!showMenu) {
-    hamburger.classList.add('open');
-    nav.classList.add('open');
-    menuNav.classList.add('open');
-    navItems.forEach(item => item.classList.add('open'));
 
-    showMenu = true;
-  } else {
-    hamburger.classList.remove('open');
-    nav.classList.remove('open');
-    menuNav.classList.remove('open');
-    navItems.forEach(item => item.classList.remove('open'));
+// Eventlisteners
+window.addEventListener('load', getCourses);
+addButton.addEventListener('click', addCourse);
 
-    showMenu = false;
-  }
+function getCourses() {
+    coursesEl.innerHTML = '';
+    // Call
+    fetch('https://studenter.miun.se/~phno1900/moment5/api/read.php')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(course => {
+            coursesEl.innerHTML +=
+            `<div class="course"><p class="box1">${course.code} </p>
+            <p class="box2"> ${course.course_name} </p>
+            <p class="box3"> ${course.progression} </p>
+            <a class="box4" href="${course.course_plan}" target="_blank">Klicka här</a>
+            <button id="${course.id}" onClick="deleteCourse(${course.id})">Radera</button>
+            <button id="${course.id}" onClick="getOneToUpdate(${course.id})">Updatera</button></div>`;
+        });
+    })
+}
+
+function deleteCourse(id) {
+    fetch('https://studenter.miun.se/~phno1900/moment5/api/delete.php?id=' + id, {
+        method: 'DELETE'
+    })
+    .then(response => response.json())
+    .then(data => {
+        getCourses();
+    })
+    .catch(error => {
+        console.log('Error: ', error);
+    })
+}
+
+function addCourse() {
+    code = code.value;
+    name = name.value;
+    prog = prog.value;
+    plan = plan.value;
+
+    let course = {'code' : code, 'course_name' : name, 'progression' : prog, 'course_plan' : plan};
+
+    fetch('https://studenter.miun.se/~phno1900/moment5/api/create.php', {
+        method: 'POST',
+        body: JSON.stringify(course)
+    })
+    .then(response => response.json())
+    .then(data => {
+        getCourses();
+    })
+    .catch(error => {
+        console.log('Error: ', error);
+    })
+
+}
+
+
+function getOneToUpdate(id) {
+    // Call
+    fetch('https://studenter.miun.se/~phno1900/moment5/api/read_single.php?id=' + id)
+    .then(response => response.json())
+    .then(updateDiv.style.display = 'block')
+    .then(course => {
+        updateDiv.innerHTML +=
+            `<form method="get">
+            <label for="code">Kurskod</label>
+            <input type="text" name="code" id="newcode" value="${course.code}"> <br>
+            <label for="name">Kursnamn</label>
+            <input type="text" name="name" id="newname" value="${course.course_name}"> <br>
+            <label for="prog">Nivå</label>
+            <input type="text" name="prog" id="newprog" value="${course.progression}"> <br>
+            <label for="plan">Kursplan</label>
+            <input type="text" name="plan" id="newplan" value="${course.course_plan}"> <br>
+            <input type="submit" id="updateButton" onClick="updateCourse(${course.id})" value="Uppdatera kurs"> <br>      
+            <input type="submit" id="closeButton" onClick="closeDiv()" value="Avbryt">
+            </form>`     
+    })
+}
+
+//closeDiv = () => {updateDiv.style.display = 'none'};
+
+function updateCourse(id) {
+    
+    let newcode = document.getElementById('newcode');
+    let newname = document.getElementById('newname');
+    let newprog = document.getElementById('newprog');
+    let newplan = document.getElementById('newplan');
+
+    newcode = newcode.value;
+    newname = newname.value;
+    newprog = newprog.value;
+    newplan = newplan.value;
+
+    let course = {'id': id, 'code' : newcode, 'course_name' : newname, 'progression' : newprog, 'course_plan' : newplan};
+
+    fetch('https://studenter.miun.se/~phno1900/moment5/api/update.php?id=' + id, {
+        method: 'PUT',
+        body: JSON.stringify(course)
+    })
+    .then(response => response.json())
+    .then(data => {
+        getCourses();
+    })
+    .catch(error => {
+        console.log('Error: ', error);
+    })
+
 }
